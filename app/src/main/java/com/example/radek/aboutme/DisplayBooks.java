515d615d3a337198ead178b653bbd7dc55189fc9
radekstasiak <radek.stasiak@gmail.com>
2015-09-16 23:19:17 +0100

    Simple code to test volley implementation

--- app/src/main/java/com/example/radek/aboutme/DisplayBooks.java
+++ app/src/main/java/com/example/radek/aboutme/DisplayBooks.java
@@ -1,19 +1,36 @@
 package com.example.radek.aboutme;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.radek.aboutme.network.APIUtils;
+import com.example.radek.aboutme.network.GoodreeadsRequest;
 
 
-public class DisplayBooks extends ActionBarActivity {
+public class DisplayBooks extends Activity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_books);
+
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        APIUtils apiUtils = new APIUtils(this);
+        apiUtils.getGoodReadsUserId();
     }
 
+    public TextView getTextView(){
+        return this.mTextView;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
