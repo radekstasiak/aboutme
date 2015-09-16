0f32edf10096077d658602a184462e202110761e
radekstasiak <radek.stasiak@gmail.com>
2015-09-16 23:17:45 +0100

    New endpoints added
    Added endpoints leading to Goodreads API.
    Added radekstasiak username.

---
+++ app/src/main/java/com/example/radek/aboutme/Constants.java
@@ -0,0 +1,16 @@
+package com.example.radek.aboutme;
+
+/**
+ * Created by Radek on 15/09/15.
+ */
+public class Constants {
+
+    public static final String GOODREADS_KEY="ES62zo1yksbkQ4sqSuMHg";
+    public static final String GOODREADS_USERNAME="radekstasiak";
+    public static final String GOODREADS_URL="https://www.goodreads.com/";
+    public static final String GOODREADS_URL_SHOWUSER="user/show/?key=%1$s&username=%2$s";
+
+
+
+
+}
